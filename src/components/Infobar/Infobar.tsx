5c2279d28deb9bf439656fe4d98a70f401f39fef
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-12-03 21:28:18 +0100

    Display current highlighted participant in our info bar

--- src/components/Infobar/Infobar.tsx
+++ src/components/Infobar/Infobar.tsx
@@ -8,6 +8,22 @@ import {useAppSelector} from "store";
 import {TooltipButton} from "components/TooltipButton/TooltipButton";
 import {ReactComponent as ShareIcon} from "assets/icon-share.svg";
 import "./Infobar.scss";
+import {UserAvatar} from "components/BoardUsers";
+
+const HighlightedParticipant = (props: {participantId?: string}) => {
+  const participant = useAppSelector((state) => [...state.participants!.others, state.participants!.self].find((participant) => participant.user.id === props.participantId));
+
+  if (!participant) {
+    return null;
+  }
+
+  return (
+    <figure className="info-bar__highlighted-participant">
+      <UserAvatar id={participant.user.id} avatar={participant.user.avatar} name={participant.user.name} />
+      <figcaption className="note__author-name">{participant.user.name}</figcaption>
+    </figure>
+  );
+};
 
 export const InfoBar = () => {
   const navigate = useNavigate();
@@ -22,12 +38,16 @@ export const InfoBar = () => {
       possibleVotes: applicationState.votings.open?.voteLimit,
       usedVotes: applicationState.votes.filter((v) => v.voting === applicationState.votings.open?.id).length,
       sharedNote: applicationState.board.data?.sharedNote,
+      highlightedParticipant: applicationState.board.data?.highlightedParticipant,
     }),
     _.isEqual
   );
 
+  console.log(state.highlightedParticipant);
+
   return ReactDOM.createPortal(
     <aside className="info-bar">
+      <HighlightedParticipant participantId={state.highlightedParticipant} />
       {state.endTime && <Timer endTime={state.endTime} />}
       {state.activeVoting && <VoteDisplay usedVotes={state.usedVotes} possibleVotes={state.possibleVotes!} />}
       {state.sharedNote && viewer.user.id !== focusInitiator?.user.id && (
